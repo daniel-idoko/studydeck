df61701b7ec32e3bce4c30bc9276bf0771a01bbd
doe-base <idokodaniel@gmail.com>
2025-10-21 02:03:29 +0100

    questions

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,11 @@ import ThemeToggle from "./components/ThemeToggle";
 import Card from "./components/Card";
 import Results from "./components/Results";
 import "./index.css";
-import questions from "questions.json";
+import questionsJson from "./questions.json";
 
 
 function App() {
+  const [questions, setQuestion] = useState(questionsJson)
   const [index, setIndex] = useState(0);
   const [showAnswer, setShowAnswer] = useState(false);
   const [grades, setGrades] = useState<number[]>([]);
